22d0f24f014776c27a4002580d54f20ad5d6de05
MandePriscille <mandepriscille1@gmail.com>
2025-07-05 21:37:48 +0100

    fix: improve dessign register

--- school/school/settings.py
+++ school/school/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'programmes',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
